6925ecdba87dddf94feea459b2b2239a3dc40f21
Crowdbotics <team@crowdbotics.com>
2021-09-10 07:59:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "wild_resonance_30431Identifier",
+  url: "https://wild_resonance_30431Identifier.botics.co",
+  api: "https://wild_resonance_30431Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
